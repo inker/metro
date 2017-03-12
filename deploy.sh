ddda342204232319c5ecad2c4c5a848582ffbc1a
Anton <anton.veselyev@gmail.com>
2017-03-12 16:21:54 +0300

    .

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ TARGET_BRANCH="gh-pages"
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
-    doCompile
+    npm run build
     exit 0
 fi
 
@@ -34,9 +34,9 @@ cd out
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+# If there are no changes (e.g. this is a README update) then just bail.
 if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
+    echo "No changes to the spec on this push; exiting."
     exit 0
 fi
 
@@ -50,7 +50,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $encrypted_199b8511517f_key -iv $encrypted_199b8511517f_iv -in ../deploy_key.enc -out deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
