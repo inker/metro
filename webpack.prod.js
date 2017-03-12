b8b514ebf1f1b4fd525ae19c57129d3175046423
Anton <anton.veselyev@gmail.com>
2017-03-12 21:26:05 +0300

    .

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,15 @@
 const base = require('./webpack.config')
 
-module.exports = {
-  ...base,
-  output: {
-    ...base.output,
-    filename: '[name].[hash].js',
-  },
-}
+module.exports = Object.assign({}, base, {
+  output: Object.assign({}, base.output, {
+    filename: '[name].[hash].js',    
+  })
+})
+
+// module.exports = {
+//   ...base,
+//   output: {
+//     ...base.output,
+//     filename: '[name].[hash].js',
+//   },
+// }
