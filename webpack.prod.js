890f5e848e3fbd313cc87e36c0721f573ab1e2c2
Anton <anton.veselyev@gmail.com>
2017-03-16 20:59:45 +0300

    typescript to ignore errors only on prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,16 @@
+const { cloneDeep }= require('lodash')
 const base = require('./webpack.config')
+
+const module = cloneDeep(base.module)
+module.rules.find(rule => rule.use === 'awesome-typescript-loader').options = {
+  ignoreDiagnostics: [2403, 2300, 2451, 2307, 2345],
+}
 
 module.exports = Object.assign({}, base, {
   output: Object.assign({}, base.output, {
     filename: '[name].[hash].js',    
-  })
+  }),
+  module,
 })
 
 // module.exports = {
