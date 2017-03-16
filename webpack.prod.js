f2a89aaf7e80fa84d58102c5da29ddf3f0527cf1
Anton <anton.veselyev@gmail.com>
2017-03-16 21:06:21 +0300

    fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,11 @@ const { cloneDeep }= require('lodash')
 const base = require('./webpack.config')
 
 const mod = cloneDeep(base.module)
-mod.rules.find(rule => rule.use === 'awesome-typescript-loader').options = {
-  ignoreDiagnostics: [2403, 2300, 2451, 2307, 2345],
+mod.rules.find(rule => rule.use === 'awesome-typescript-loader').use = {
+  loader: 'awesome-typescript-loader',
+  options: {
+    ignoreDiagnostics: [2403, 2300, 2451, 2307, 2345],
+  },
 }
 
 module.exports = Object.assign({}, base, {
