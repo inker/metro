bee09c328304858015297720d7bcf2256db4238c
Anton <anton.veselyev@gmail.com>
2017-07-13 21:11:48 +0300

    .

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -63,5 +63,5 @@ module.exports = env => [
     // sourceMap: true, // retains sourcemaps for typescript
   }),
 
-  env === 'test' && new BundleAnalyzerPlugin(),
+  env === 'analyze' && new BundleAnalyzerPlugin(),
 ].filter(item => item)
