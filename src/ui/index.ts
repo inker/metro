6b0d49c338c9548e80dbda8dfcecc1fa7bf8c798
inker <anton.veselyev@gmail.com>
2017-09-30 22:03:27 +0300

    .

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -7,7 +7,7 @@ import { calculateGeoMedian } from '../util/geo'
 import { tr } from '../i18n'
 
 import * as Icons from './Icons'
-import * as tilelayers from './tileLayers'
+import * as tilelayers from './tilelayers'
 
 export { default as MapEditor } from './MapEditor'
 export { default as FAQ } from './FAQ'
