4988ab890048a7bf0c8b3f19c9e3bdb1e1a27e04
Anton <anton.veselyev@gmail.com>
2017-11-01 01:04:41 +0300

    .

--- src/ui/alertify/index.ts
+++ src/ui/alertify/index.ts
@@ -3,11 +3,10 @@ import * as alertify from 'alertifyjs'
 import 'alertifyjs/build/css/alertify.css'
 import 'alertifyjs/build/css/themes/default.css'
 
-import { transitionEnd } from '../../util/events'
-
 import './styles.css'
 
 export default alertify
+export { default as getPassword } from './getPassword'
 
 export const prompt = (...args: any[]) => new Promise<string | null>((resolve, reject) => {
     alertify.prompt(...args, (ev, value: string) => {
@@ -16,17 +15,6 @@ export const prompt = (...args: any[]) => new Promise<string | null>((resolve, r
         resolve(null)
     })
 })
-
-export async function getPassword(...args: any[]) {
-    const input = document.querySelector('.ajs-input') as HTMLInputElement
-    input.type = 'password'
-    const val = await prompt(...args)
-    const dimmer = document.querySelector('.ajs-dimmer')
-    if (dimmer) {
-        transitionEnd(dimmer).then(() => input.type = 'text')
-    }
-    return val
-}
 
 export const confirm = (text: string) => new Promise<boolean>((resolve) => {
     alertify.confirm(text, () => {
