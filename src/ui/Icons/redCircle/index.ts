86c0d52c91478129c3a6bbabf64da9c349386579
Anton <anton.veselyev@gmail.com>
2017-11-05 16:17:00 +0300

    .

--- src/ui/Icons/redCircle/index.ts
+++ src/ui/Icons/redCircle/index.ts
@@ -1,17 +1,17 @@
 import { point, icon } from 'leaflet'
 import { svgToDataUrl } from 'svgio'
 
-import { createSVGElement, makeCircle } from '../../util/svg'
+import { createSVGElement, makeCircle } from '../../../util/svg'
+
+import styles from './styles.pcss'
 
 export default (radius: number) => {
     const root = createSVGElement('svg')
     root.setAttribute('width', '100')
     root.setAttribute('height', '100')
+
     const ci = makeCircle(point(50, 50), 40)
-    const { style } = ci
-    style.stroke = 'red'
-    style.strokeWidth = '20px'
-    style.fill = 'white'
+    ci.classList.add(styles['red-circle'])
     root.appendChild(ci)
 
     const diameter = radius * 2
