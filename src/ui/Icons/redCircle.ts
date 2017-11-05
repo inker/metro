739afa926575e9b2cdd3abe7aad36ce77f61a40e
Anton <anton.veselyev@gmail.com>
2017-11-05 16:25:33 +0300

    .

--- src/ui/Icons/redCircle.ts
+++ src/ui/Icons/redCircle.ts
@@ -3,14 +3,21 @@ import { svgToDataUrl } from 'svgio'
 
 import { createSVGElement, makeCircle } from '../../util/svg'
 
+const SVG_SIZE = 100
+
 export default (radius: number) => {
     const root = createSVGElement('svg')
-    root.setAttribute('width', '100')
-    root.setAttribute('height', '100')
-    const ci = makeCircle(point(50, 50), 40)
+    const sizeStr = SVG_SIZE.toString()
+    root.setAttribute('width', sizeStr)
+    root.setAttribute('height', sizeStr)
+
+    const halfSize = SVG_SIZE / 2
+    const offset = halfSize / 10
+
+    const ci = makeCircle(point(halfSize, halfSize), halfSize - offset)
     const { style } = ci
     style.stroke = 'red'
-    style.strokeWidth = '20px'
+    style.strokeWidth = `${offset * 2}px`
     style.fill = 'white'
     root.appendChild(ci)
 
