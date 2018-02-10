8709c8e1a5469813dd47012308d652cb4be1a8e4
Anton <anton.veselyev@gmail.com>
2018-02-10 17:09:06 +0300

    restore UglifyJsPlugin

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -102,26 +102,26 @@ module.exports = env => [
     },
   ]),
 
-  // env !== 'dev' && new UglifyJsPlugin({
-  //   uglifyOptions: {
-  //     compress: {
-  //       ecma: 6,
-  //       warnings: true,
-  //       dead_code: true,
-  //       properties: true,
-  //       unused: true,
-  //       join_vars: true,
-  //       drop_console: true,
-  //     },
-  //     mangle: {
-  //       safari10: true,
-  //     },
-  //     output: {
-  //       comments: false,
-  //     },
-  //   },
-  //   // sourceMap: true, // retains sourcemaps for typescript
-  // }),
+  env !== 'dev' && new UglifyJsPlugin({
+    uglifyOptions: {
+      compress: {
+        ecma: 6,
+        warnings: true,
+        dead_code: true,
+        properties: true,
+        unused: true,
+        join_vars: true,
+        drop_console: true,
+      },
+      mangle: {
+        safari10: true,
+      },
+      output: {
+        comments: false,
+      },
+    },
+    // sourceMap: true, // retains sourcemaps for typescript
+  }),
 
   env === 'analyze' && new BundleAnalyzerPlugin(),
 ].filter(item => item)
