4b13330aa72cb9a9e6a94ee9f7f1fcb5bccc9420
inker <anton.veselyev@gmail.com>
2018-02-18 06:11:25 +0300

    platform offset

--- src/Metro/getPlatformPositions.ts
+++ src/Metro/getPlatformPositions.ts
@@ -24,11 +24,11 @@ const getPositionsMemoized = memoize(
 export default (
   platform: Platform,
   getPlatformPosition: (platform: Platform) => Point,
-  getPlatformOffset: (position: Point) => Map<any, number> | null,
+  getPlatformOffset: (platform: Platform) => Map<any, number> | null,
   getFirstWhisker: (platform: Platform) => Point,
 ) => {
   const pos = getPlatformPosition(platform)
-  const offsetsMap = getPlatformOffset(pos)
+  const offsetsMap = getPlatformOffset(platform)
   if (!offsetsMap) {
     return pos
   }
