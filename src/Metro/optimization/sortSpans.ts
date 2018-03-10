12a7a9794d9756fc160424fc687543e2c821a07c
inker <anton.veselyev@gmail.com>
2018-03-10 18:05:06 +0300

    intersecting a parallel batch reduced penalty

--- src/Metro/optimization/sortSpans.ts
+++ src/Metro/optimization/sortSpans.ts
@@ -10,11 +10,12 @@ import Span from '../../network/Span'
 
 export default (spans: Span[], parallelSpans: Span[][]) => {
   const map = new WeakMap<Span, number>()
-  for (const s of spans) {
+  for (let i = 0; i < spans.length; ++i) {
+    const s = spans[i]
     const parallels = parallelSpans.find(ss => ss.includes(s))
     const numParallels = parallels ? parallels.length : 1
     const eFactor = s.routes[0].line === 'E' ? 0 : Infinity
-    map.set(s, numParallels + eFactor)
+    map.set(s, numParallels + i * 0.0001 + eFactor)
   }
   const sortedSpans = orderBy(spans, s => tryGetFromMap(map, s), 'desc')
   spans.splice(0, spans.length, ...sortedSpans)
